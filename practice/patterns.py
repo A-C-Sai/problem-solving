bd9812e2a91abb5ae5eb4869623a5eee8ea3d324
Sai C Akella <sai.c.akella@gmail.com>
2024-06-27 04:32:20 +1000

    patterns

--- practice/patterns.py
+++ practice/patterns.py
@@ -154,8 +154,141 @@ def ut_inverted(N):
         print("* "*(N-i)+"  "*i)
 
 
+def pattern6(N):
+    """
+
+    e.g. N = 5
+
+      j  1 2 3 4 5
+    i    
+    1    1
+    2    1 2
+    3    1 2 3
+    4    1 2 3 4
+    5    1 2 3 4 5 
+
+    Key : output numbers <= i
+
+    """
+
+    # for i in range(1,N+1):
+    #     for j in range(1,i+1):
+    #         print(j,end=" ")
+    #     print()
+
+    for i in range(1,N+1):
+        print(*[str(j) for j in range(1,i+1)])
+
+
+def pattern7(N):
+    """
+
+    e.g. N = 5
+
+      j  1 2 3 4 5
+    i    
+    1    1
+    2    2 2
+    3    3 3 3
+    4    4 4 4 4
+    5    5 5 5 5 5 
+
+    Key : output "i" i times for each i
+
+    """
+
+    # for i in range(1,N+1):
+    #     for _ in range(1,i+1):
+    #         print(i,end=" ")
+    #     print()
+
+    for i in range(1,N+1):
+        print(*list(str(i)*i))
+
+
+def pattern8(N):
+    """
+
+    e.g. N = 5
+
+      j  0 1 2 3 4
+    i    
+    0    1 2 3 4 5
+    1    1 2 3 4
+    2    1 2 3
+    3    1 2 
+    4    1 
+
+    Key : output [1 to N-i] for every row
+
+    """
+    
+    for i in range(N):
+        for j in range(1,N-i+1):
+            print(j,end=" ")
+        print()
+
+
+def pattern9(N):
+    """
+
+    e.g. N = 5
+
+      j  1 2 3 4 5
+    i    
+    1        *
+    2      * * *
+    3    * * * * * 
+ 
+
+    Key : 
+        rows = N
+        columns = (N*2)-1
+        * : N-i < j < N+i
+        space elsewhere
+
+    """
+    for i in range(1,N+1):
+        for j in range(1,N*2):
+            if(N-i < j < N+i):
+                print("*",end=" ")
+            else:
+                print(" ",end=" ")
+        print()
+
+
+
+def pattern10(N):
+    """
+
+    e.g. N = 5
+
+      j  1 2 3 4 5
+    i    
+    1    * * * * *
+    2      * * *
+    3        * 
+ 
+
+    Key : 
+        rows = N
+        columns = (N*2)-1
+        * : i-1 < j < 2*N-i+1
+        space elsewhere
+
+    """
+    for i in range(1,N+1):
+        for j in range(1,N*2):
+            if(i-1 < j < 2*N-i+1):
+                print("*",end=" ")
+            else:
+                print(" ",end=" ")
+        print()
+
+
+
 
 if __name__ == "__main__":
 
     for line in sys.stdin:
-        square(int(line.strip()))
+        pattern10(int(line.strip()))
