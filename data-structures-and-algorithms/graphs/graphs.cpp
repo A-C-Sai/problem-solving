206ff3dd456c07cf545b22f23ffb841029e47c1b
Sai C Akella <sai.c.akella@gmail.com>
2024-07-17 14:11:16 +1000

    implemented BFS graph traversal

--- data-structures-and-algorithms/graphs/graphs.cpp
+++ data-structures-and-algorithms/graphs/graphs.cpp
@@ -1,11 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void BFS(vector<vector<int>> &graph, int entryNode) // O(V+2E)
+{
+    vector<int> visited(graph.size(), 0);
+    queue<int> q; // nodes who are yet to be explored
+
+    cout << "BFS " << "starting on Node " << entryNode << " : ";
+
+    // initial steps
+    cout << entryNode << " ";
+    visited[entryNode]++;
+    q.push(entryNode);
+
+    while (!q.empty()) // visit all nodes
+    {
+        // explore its neighbors
+        for (auto i = graph[q.front()].begin(); i != graph[q.front()].end(); i++)
+        {
+            if (!visited[(*i)])
+            {
+                cout << (*i) << " ";
+                visited[(*i)]++;
+                q.push((*i));
+            }
+        }
+        q.pop();
+    }
+    cout << endl;
+}
+
 int main()
 {
     int nodes, edges; // total # of nodes and edges in graph
     cin >> nodes >> edges;
-    vector<int> graph[nodes + 1]; // adjacency list representation of undirected graph i.e. array of vectors
+    vector<vector<int>> graph(nodes + 1); // adjacency list representation of undirected graph i.e. array of vectors
 
     int n1, n2; // edge pair
     for (int i = 0; i < edges; i++)
@@ -14,5 +43,10 @@ int main()
         graph[n1].push_back(n2);
         graph[n2].push_back(n1);
     }
+
+    int entryNode;
+    cin >> entryNode;
+    BFS(graph, entryNode);
+
     return 0;
 }
